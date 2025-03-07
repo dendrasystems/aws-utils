07136687de53b2b96a9b4dbc37eff8cb9b459796
Samir Shah <samir.shah@dendra.io>
2025-03-07 08:02:21 +0300

    feat: add package scaffolding and initial set of s3 utils

---
+++ src/aws_utils/s3.py
@@ -0,0 +1,161 @@
+import functools
+import os
+from typing import Generator, NamedTuple
+import boto3
+from boto3.s3 import transfer
+from urllib.parse import urlsplit
+from botocore.exceptions import ClientError
+
+
+@functools.lru_cache
+def _get_client():
+    return boto3.client("s3")
+
+
+class S3UrlParts(NamedTuple):
+    bucket: str
+    key: str
+
+
+def parse_s3_url(url: str) -> S3UrlParts:
+    """
+    Parses an s3 URL with either an s3:// or https:// scheme, to extract the bucket and key.
+
+    Returns:
+        A tuple of bucket and key
+
+    Raises:
+      ValueError: If the url is not a valid S3 URL
+    """
+    parts = urlsplit(url)
+    if parts.scheme not in ("https", "s3"):
+        raise ValueError(f"{url} is not a valid S3 URL")
+
+    if parts.scheme == "s3":
+        return S3UrlParts(bucket=parts.netloc, key=parts.path.lstrip("/"))
+    if parts.netloc.endswith(".s3.amazonaws.com"):
+        return S3UrlParts(
+            bucket=parts.netloc.rsplit(".s3.", 1)[0], key=parts.path.lstrip("/")
+        )
+    if parts.netloc.startswith("s3-") and parts.netloc.endswith(".amazonaws.com"):
+        bucket, key = parts.path.split("/", 2)[1:]
+        return S3UrlParts(bucket=bucket, key=key.lstrip("/"))
+    if parts.netloc.endswith(".amazonaws.com"):
+        return S3UrlParts(bucket=parts.netloc.split(".")[0], key=parts.path.lstrip("/"))
+
+    raise ValueError(f"{url} is not a valid S3 URL")
+
+
+def make_s3_url(bucket: str, key: str):
+    """
+    Converts a bucket and key into an s3:// URL.
+    """
+    return f"s3://{bucket}/{key}"
+
+
+def make_https_url(bucket: str, key: str, aws_region: str):
+    """
+    Converts a bucket and key into an https:// URL.
+    """
+    return f"https://{bucket}.s3.{aws_region}.amazonaws.com/{key}"
+
+
+def iter_keys(
+    bucket: str, prefix: str, max_keys: int | None = None, client=None
+) -> Generator[dict, None, None]:
+    """Yields objects in an S3 bucket for a given prefix.
+
+    Args:
+        bucket:             S3 bucket name
+        prefix:             Key prefix
+        min_object_size:    Minimum object size (in bytes) to keep
+        max_keys:           Limits results to this size
+    """
+    client = client or _get_client()
+    key_count = 0
+    kwargs = {"Bucket": bucket, "Prefix": prefix}
+
+    while True:
+        resp = client.list_objects_v2(**kwargs)
+        if resp["KeyCount"] == 0:
+            break
+
+        for obj in resp["Contents"]:
+            key_count += 1
+            yield obj
+
+            if max_keys and key_count >= max_keys:
+                break
+
+        if resp["IsTruncated"]:
+            kwargs["ContinuationToken"] = resp["NextContinuationToken"]
+        else:
+            break
+
+
+def sync_object(src: dict, dest: dict, client=None) -> None:
+    """
+    Copy an object from one S3 location to another but only if the
+    source is newer than the destination.
+
+    Args:
+        src: the source object URL in {"Bucket": "", "Key": ""} format
+        dest: the source object URL in {"Bucket": "", "Key": ""} format
+    """
+    client = client or _get_client()
+
+    # Check if the destination exists
+    try:
+        resp = client.head_object(**dest)
+        dest_last_modified = resp["LastModified"]
+    except ClientError:
+        # This assumes a 404 - could also be some other issue, in which case the copy will fail anyway
+        pass
+    else:
+        # Check if the source is newer
+        resp = client.head_object(**src)
+        src_last_modified = resp["LastModified"]
+        if src_last_modified <= dest_last_modified:
+            return
+
+    client.copy(CopySource=src, **dest)
+
+
+def upload_file(
+    source_path: str,
+    bucket: str,
+    key: str,
+    content_type=None,
+    content_disposition=None,
+    client=None,
+):
+    """
+    Uploads a file on disk to S3, setting ContentType and ContentDisposition attributes if supplied.
+    """
+    client = client or _get_client()
+
+    extra_args = {}
+    if content_type:
+        extra_args["ContentType"] = content_type
+    if content_disposition:
+        extra_args["ContentDisposition"] = content_type
+
+    client.upload_file(source_path, Bucket=bucket, Key=key, ExtraArgs=extra_args)
+
+
+def upload_dir(source_path: str, bucket: str, prefix: str, client=None):
+    """
+    Uploads all files in a directory to the specified bucket and prefix.
+    """
+    client = client or _get_client()
+    manager = transfer.create_transfer_manager(
+        client, transfer.TransferConfig(use_threads=True, max_concurrency=20)
+    )
+    for root, _, files in os.walk(source_path):
+        for filename in files:
+            local_path = os.path.join(root, filename)
+            relative_path = os.path.relpath(local_path, source_path)
+            s3_path = os.path.join(prefix, relative_path)
+            manager.upload(local_path, bucket, s3_path)
+    # Wait for all uploads to complete
+    manager.shutdown()
---
+++ tests/test_s3.py
@@ -0,0 +1,193 @@
+import datetime
+import os
+from unittest import mock
+import pytest
+
+from aws_utils import s3
+from botocore.exceptions import ClientError
+
+
+class TestParseS3Url:
+    @pytest.mark.parametrize(
+        ("url", "expected"),
+        [
+            ("s3://bucket/key", s3.S3UrlParts("bucket", "key")),
+            ("https://bucket.s3.amazonaws.com/key", s3.S3UrlParts("bucket", "key")),
+            (
+                "https://s3-region.amazonaws.com/bucket/key",
+                s3.S3UrlParts("bucket", "key"),
+            ),
+        ],
+    )
+    def test_valid(self, url, expected):
+        assert s3.parse_s3_url(url) == expected
+
+    @pytest.mark.parametrize(
+        "url",
+        [
+            "https://google.com",
+            "ftp://example.com",
+        ],
+    )
+    def test_invalid(self, url):
+        with pytest.raises(ValueError):
+            s3.parse_s3_url(url)
+
+
+class TestMakeS3Url:
+    def test_func(self):
+        assert (
+            s3.make_s3_url(bucket="test", key="foo/bar.jpg") == "s3://test/foo/bar.jpg"
+        )
+
+
+class TestMakeHttpsUrl:
+    def test_func(self):
+        assert (
+            s3.make_https_url(bucket="test", key="foo/bar.jpg", aws_region="eu-west-1")
+            == "https://test.s3.eu-west-1.amazonaws.com/foo/bar.jpg"
+        )
+
+
+class TestIterKeys:
+    def test_no_keys_found(self):
+        mock_client = mock.MagicMock()
+        mock_client.list_objects_v2.return_value = {"KeyCount": 0}
+        generator = s3.iter_keys(bucket="test", prefix="images/", client=mock_client)
+        assert list(generator) == []
+
+    def test_untruncated_response(self):
+        mock_client = mock.MagicMock()
+        mock_client.list_objects_v2.return_value = {
+            "KeyCount": 10,
+            "IsTruncated": False,
+            "Contents": [{}] * 10,
+        }
+        generator = s3.iter_keys(bucket="test", prefix="images/", client=mock_client)
+        assert len(list(generator)) == 10
+
+    def test_truncated_response_is_fetched_in_pages(self):
+        mock_client = mock.MagicMock()
+        mock_client.list_objects_v2.side_effect = [
+            {
+                "KeyCount": 10,
+                "IsTruncated": True,
+                "NextContinuationToken": "next-token",
+                "Contents": [{}] * 10,
+            },
+            {"KeyCount": 10, "IsTruncated": False, "Contents": [{}] * 5},
+        ]
+        generator = s3.iter_keys(bucket="test", prefix="images/", client=mock_client)
+        assert len(list(generator)) == 15
+        assert mock_client.list_objects_v2.call_count == 2
+
+    def test_limits_results__max_keys(self):
+        mock_client = mock.MagicMock()
+        mock_client.list_objects_v2.side_effect = [
+            {
+                "KeyCount": 10,
+                "IsTruncated": True,
+                "NextContinuationToken": "next-token",
+                "Contents": [{}] * 10,
+            },
+            {"KeyCount": 10, "IsTruncated": False, "Contents": [{}] * 5},
+        ]
+        generator = s3.iter_keys(
+            bucket="test", prefix="images/", max_keys=11, client=mock_client
+        )
+        assert len(list(generator)) == 11
+
+
+class TestSyncObject:
+    def test_copy_dest_does_not_exist(self):
+        mock_client = mock.MagicMock()
+        mock_client.head_object.side_effect = [
+            ClientError(error_response={}, operation_name="HeadObject"),
+            {"LastModified": datetime.datetime(2015, 1, 1)},
+        ]
+        s3.sync_object(
+            {"Bucket": "src", "Key": "item.png"},
+            {"Bucket": "dest", "Key": "item.png"},
+            client=mock_client,
+        )
+        mock_client.copy.assert_called_once_with(
+            CopySource={"Bucket": "src", "Key": "item.png"},
+            Bucket="dest",
+            Key="item.png",
+        )
+
+    def test_copy_dest_older_than_src(self):
+        mock_client = mock.MagicMock()
+        mock_client.head_object.side_effect = [
+            {"LastModified": datetime.datetime(2015, 1, 1)},
+            {"LastModified": datetime.datetime(2016, 1, 1)},
+        ]
+        s3.sync_object(
+            {"Bucket": "src", "Key": "item.png"},
+            {"Bucket": "dest", "Key": "item.png"},
+            client=mock_client,
+        )
+        mock_client.copy.assert_called_once_with(
+            CopySource={"Bucket": "src", "Key": "item.png"},
+            Bucket="dest",
+            Key="item.png",
+        )
+
+    def test_copy_dest_newer_than_src(self):
+        mock_client = mock.MagicMock()
+        mock_client.head_object.side_effect = [
+            {"LastModified": datetime.datetime(2016, 1, 1)},
+            {"LastModified": datetime.datetime(2015, 1, 1)},
+        ]
+        s3.sync_object(
+            {"Bucket": "src", "Key": "item.png"},
+            {"Bucket": "dest", "Key": "item.png"},
+            client=mock_client,
+        )
+        mock_client.copy.assert_not_called()
+
+
+class TestUploadFile:
+    def test_sets_extra_args(self):
+        mock_client = mock.MagicMock()
+        s3.upload_file(
+            source_path="some/file.png",
+            bucket="test",
+            key="output/file.png",
+            content_type="image/png",
+            client=mock_client,
+        )
+        mock_client.upload_file.assert_called_once_with(
+            "some/file.png",
+            Bucket="test",
+            Key="output/file.png",
+            ExtraArgs={"ContentType": "image/png"},
+        )
+
+
+class TestUploadDir:
+    def test_upload_call(self, mocker):
+        mocker.patch.object(
+            os,
+            "walk",
+            return_value=[
+                ("./src", ["dir1", "dir2"], []),
+                ("./src/dir1", [], ["file1.txt", "file2.txt"]),
+                ("./src/dir2", [], ["file3.txt"]),
+            ],
+        )
+        mock_manager = mock.MagicMock()
+        mocker.patch.object(
+            s3.transfer, "create_transfer_manager", return_value=mock_manager
+        )
+        s3.upload_dir("./src", "test", "output/")
+
+        assert mock_manager.upload.call_count == 3
+        mock_manager.upload.assert_has_calls(
+            [
+                mock.call("./src/dir1/file1.txt", "test", "output/dir1/file1.txt"),
+                mock.call("./src/dir1/file2.txt", "test", "output/dir1/file2.txt"),
+                mock.call("./src/dir2/file3.txt", "test", "output/dir2/file3.txt"),
+            ]
+        )
+        mock_manager.shutdown.assert_called_once()
